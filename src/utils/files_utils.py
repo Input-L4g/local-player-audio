1e7f125f3d0bf846a6173fbf257d0e3fb5891ff2
Input-L4g <diogo.antunes0328@gmail.com>
2025-12-16 19:40:36 -0300

    feat: Criar função clear_file

--- src/utils/files_utils.py
+++ src/utils/files_utils.py
@@ -2,3 +2,9 @@
 Esse módulo contém funções utilitárias para
 manipulação de arquivos.
 """
+from src.core.type_hints import PathType
+
+def clear_file(file_path: PathType) -> None:
+    """Limpa o contúdo de um arquivo."""
+    with open(file_path, "w", encoding="utf-8") as _:
+        pass
