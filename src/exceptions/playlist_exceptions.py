dd87d937fdafbe14f0e21454a5bc7a410770e3f2
Input-L4g <diogo.antunes0328@gmail.com>
2025-12-16 16:25:59 -0300

    feat: Criar classes Exception para error internos da Playlist

---
+++ src/exceptions/playlist_exceptions.py
@@ -0,0 +1,23 @@
+"""
+Esse módulo contém exceções referentes
+a classe Playlist.
+"""
+from src.core.type_hints import AudioPathType
+
+class TrackExistsError(Exception):
+    """Representa uma exceção quando uma trilha já existe na playlist."""
+    def __init__(self, track_path: AudioPathType) -> None:
+        self.message: str = f"A trilha '{track_path}' já existe."
+        super().__init__(self.message)
+
+class TrackNotExistsError(Exception):
+    """Representa uma exceção quando uma trilha não existe na playlist."""
+    def __init__(self, track_path: AudioPathType) -> None:
+        self.message: str = f"A trilha '{track_path}' não existe."
+        super().__init__(self.message)
+
+class PlaylistEmptyError(Exception):
+    """Representa uma exceção quando a playlist está vazia."""
+    def __init__(self) -> None:
+        self.message: str = "A playlist está vazia."
+        super().__init__(self.message)
