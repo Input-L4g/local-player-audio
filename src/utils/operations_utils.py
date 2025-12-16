f7f9b0de80806a5197c7e50d04331e23bdfdff31
Input-L4g <diogo.antunes0328@gmail.com>
2025-12-16 13:29:09 -0300

    feat: Adicionar função increment_index

--- src/utils/operations_utils.py
+++ src/utils/operations_utils.py
@@ -2,3 +2,18 @@
 Esse módulo contém funções utilitáras referentes
 a cálculos ou operações.
 """
+
+def increment_index(
+    index: int,
+    len_list: int,
+    increment_value: int = 1
+) -> int:
+    """
+    Incrementa em um index mantendo-o no tamanho da lista.
+    Valores positivos incrementam, negativos decrementam.
+    """
+    if increment_value > 0:
+        return (index + increment_value) % len_list
+    if increment_value < 0:
+        return (len_list + increment_value) % len_list
+    return index
