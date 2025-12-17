f87a788d8c32b813422b2ce54093f8c47211a82c
Input-L4g <diogo.antunes0328@gmail.com>
2025-12-16 22:21:03 -0300

    test: Adicionar teste para increment_index

--- tests/test_operations_utils.py
+++ tests/test_operations_utils.py
@@ -3,3 +3,25 @@ Esse módulo contém testes unitários e
 automatizados referente as funções
 do módulo operations_utils.py
 """
+from src.utils import operations_utils
+
+def test_increment_index() -> None:
+    """Testa a função `increment_index`."""
+    index = 0
+    len_list = 3
+    assert index == 0, f"Index: {index}"
+    index = operations_utils.increment_index(index, len_list)
+    assert index == 1, f"Index: {index}"
+    index = operations_utils.increment_index(index, len_list)
+    assert index == 2, f"Index: {index}"
+    index = operations_utils.increment_index(index, len_list)
+    assert index == 0, f"Index: {index}"
+
+    index = 0
+    assert index == 0, f"Index: {index}"
+    index = operations_utils.increment_index(index, len_list, -1)
+    assert index == 2, f"Index: {index}"
+    index = operations_utils.increment_index(index, len_list, -1)
+    assert index == 1, f"Index: {index}"
+    index = operations_utils.increment_index(index, len_list, -1)
+    assert index == 0, f"Index: {index}"
